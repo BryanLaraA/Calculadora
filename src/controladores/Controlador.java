50d50c7a6d0589442f65d6549dfa94d59b50bb56
Bryan <bryanlaraarguello@gmail.com>
2026-08-04 14:37:09 -0600

    Creacion del constructor del controldor

--- src/controladores/Controlador.java
+++ src/controladores/Controlador.java
@@ -7,4 +7,23 @@ public class Controlador {
     
     private frmInterfaz frminterfaz;
     private Calculadora calculadora;
+    private double primervalor;
+    private boolean segundovalor;
+    private String operacion;
+
+    public Controlador(frmInterfaz frminterfaz, Calculadora calculadora, String operacion) {
+        this.frminterfaz = frminterfaz;
+        this.calculadora = calculadora;
+        this.primervalor = 0;
+        this.segundovalor = false;
+        this.operacion = "";
+    }
+    
+    
+    
+
+   
+    
+    
+    
 }
