32d24e7cbdfa9346a2f6b4afe619052805945a87
isaac <isaacpr11371@gmail.com>
2026-08-04 13:58:37 -0600

    Creacion del paquete excepciones
    Se crea el paquete de excepciones con campo vacio y division por cero

---
+++ src/excepciones/IlegalExcepcionCampoVacio.java
@@ -0,0 +1,7 @@
+package excepciones;
+
+public class IlegalExcepcionCampoVacio extends Exception{
+    public IlegalExcepcionCampoVacio() {
+        super("Ingrese datos, Campo Vacio");
+    }
+}
---
+++ src/excepciones/IlegalExcepcionDivisionCero.java
@@ -0,0 +1,8 @@
+package excepciones;
+
+public class IlegalExcepcionDivisionCero extends ArithmeticException{
+
+    public IlegalExcepcionDivisionCero() {
+        System.out.println("Division ilegal, no se puede dividir por cero");
+    }
+}
