63678eb3ce98b04bc5baa6d6ebb2062099195c4b
isaac <isaacpr11371@gmail.com>
2026-08-04 14:19:26 -0600

    Modificacion de estructuras de las excepciones
    se modifican las excepciones

--- src/excepciones/IlegalExcepcionDivisionCero.java
+++ src/excepciones/IlegalExcepcionDivisionCero.java
@@ -1,7 +1,6 @@
 package excepciones;
 
 public class IlegalExcepcionDivisionCero extends ArithmeticException{
-
     public IlegalExcepcionDivisionCero() {
         System.out.println("Division ilegal, no se puede dividir por cero");
     }
