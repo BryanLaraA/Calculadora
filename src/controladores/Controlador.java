228827760a82abd74766c5fcb703f47ecb6b340e
Bryan <bryanlaraarguello@gmail.com>
2026-08-04 14:21:21 -0600

    Creacion del controlador

---
+++ src/controladores/Controlador.java
@@ -0,0 +1,10 @@
+package controladores;
+
+import Interfaz.frmInterfaz;
+import clasesLogicas.Calculadora;
+
+public class Controlador {
+    
+    private frmInterfaz frminterfaz;
+    private Calculadora calculadora;
+}
